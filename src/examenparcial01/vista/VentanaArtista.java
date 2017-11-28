ba106f3989548122a7206be076ecf1c6cb085ee7
Jose <Jose@Jose>
2017-11-28 08:27:29 -0500

    Creacion de Eventos y ventanas

---
+++ src/examenparcial01/vista/VentanaArtista.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenparcial01.vista;
+
+import examenparcial01.controlador.GestionDato;
+import javax.swing.JInternalFrame;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Jose
+ */
+public class VentanaArtista extends JInternalFrame {
+    
+    private GestionDato gestionDato;
+    
+    
+    
+    
+}
