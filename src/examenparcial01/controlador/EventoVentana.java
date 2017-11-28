80c10ce8b8dccf2bbd63293c457398c919f1f36d
johne <johne@172.16.220.10>
2017-11-28 14:26:00 -0500

    Ventana Principal/Evento

---
+++ src/examenparcial01/controlador/EventoVentana.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenparcial01.controlador;
+
+import examenparcial01.vista.Ventana;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author johne
+ */
+public class EventoVentana implements ActionListener
+{
+    private Ventana Ventana;
+
+    public EventoVentana(Ventana Ventana) {
+        this.Ventana = Ventana;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+         if (e.getSource().equals(this.Ventana.getMenuItemList().get(0))) 
+         {
+             System.err.println("Agregar ventana Asistente");
+         }
+         if (e.getSource().equals(this.Ventana.getMenuItemList().get(1))) 
+         {
+             System.err.println("Agregar ventana Artista");
+         }
+           if (e.getSource().equals(this.Ventana.getMenuItemList().get(2))) 
+         {
+             System.err.println("Agregar ventana Presentacion");
+         }
+           if (e.getSource().equals(this.Ventana.getMenuItemList().get(3))) 
+         {
+             System.err.println("Agregar ventana Festival");
+         }
+             if (e.getSource().equals(this.Ventana.getMenuItemList().get(4))) 
+         {
+             System.err.println("Agregar ventana Boleto");
+         }
+    }
+
+    public Ventana getVentana() {
+        return Ventana;
+    }
+
+    public void setVentana(Ventana Ventana) {
+        this.Ventana = Ventana;
+    }
+    
+    
+}
--- src/examenparcial01/vista/Ventana.java
+++ src/examenparcial01/vista/Ventana.java
@@ -5,8 +5,10 @@
  */
 package examenparcial01.vista;
 
+import examenparcial01.controlador.EventoVentana;
 import examenparcial01.controlador.GestionDato;
 import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -41,39 +43,86 @@ public class Ventana extends JFrame
         this.barraMenu= new JMenuBar();
         this.menuList=new ArrayList<JMenu>();// llamado al array
         this.menuList.add(new JMenu("Participante"));// ingreso de datos en el array
-        this.menuList.add(new JMenu("Festival"));// ingreso de datos en el array
-        this.menuList.add(new JMenu(""));// ingreso de datos en el array
-        this.menuItemList= new ArrayList<JMenuItem>();
-        this.menuItemList.add(new JMenuItem("Presentacion"));
-        this.menuItemList.add(new JMenuItem("Boleto"));
-        this.menuItemList.add(new JMenuItem("Artista"));
-        this.menuItemList.add(new JMenuItem("Festival"));
+        this.menuList.add(new JMenu("Actividad"));// ingreso de datos en el array
+        this.menuList.add(new JMenu("Imprimir"));// ingreso de datos en el array
 
-        this.menuItemList.add(new JMenuItem("Consultar Carrera"));
-        this.menuItemList.add(new JMenuItem("Consultar Estudiante"));
-        
-     
+
+        this.menuItemList= new ArrayList<JMenuItem>();
+        this.menuItemList.add(new JMenuItem("Agregar Asistente"));
+        this.menuItemList.add(new JMenuItem("Agregar Artista"));
+        this.menuItemList.add(new JMenuItem("Agregar Presentacion"));
+        this.menuItemList.add(new JMenuItem("Agregar Festival"));
+        this.menuItemList.add(new JMenuItem("Boleto"));
+
         
         this.setContentPane(this.escritorio);
         this.setJMenuBar(barraMenu);
         
-        this.barraMenu.add(this.menuItemList.get(0));
-        this.barraMenu.add(this.menuItemList.get(1));
-        this.barraMenu.add(this.menuItemList.get(2));
-        this.barraMenu.add(this.menuItemList.get(3));
         this.barraMenu.add(this.menuList.get(0));
-        this.menuList.get(0).add(this.menuItemList.get(4));
-        this.menuList.get(0).add(this.menuItemList.get(5));
-       
+        this.menuList.get(0).add(this.menuItemList.get(0));
+        this.menuList.get(0).add(this.menuItemList.get(1));
         
+        this.barraMenu.add(this.menuList.get(1));
+        this.menuList.get(1).add(this.menuItemList.get(2));
+        this.menuList.get(1).add(this.menuItemList.get(3));
         
-        /*this.menuItemList.get(0).addActionListener(new EventoVentana(this));
+        this.barraMenu.add(this.menuList.get(2));
+        this.menuList.get(2).add(this.menuItemList.get(4));
+        
+        this.menuItemList.get(0).addActionListener(new EventoVentana(this));
         this.menuItemList.get(1).addActionListener(new EventoVentana(this));
         this.menuItemList.get(2).addActionListener(new EventoVentana(this));
         this.menuItemList.get(3).addActionListener(new EventoVentana(this));
         this.menuItemList.get(4).addActionListener(new EventoVentana(this));
-        this.menuItemList.get(5).addActionListener(new EventoVentana(this));
-        */
 
     }
+
+    public GestionDato getGestionDato() {
+        return gestionDato;
+    }
+
+    public void setGestionDato(GestionDato gestionDato) {
+        this.gestionDato = gestionDato;
+    }
+
+    public JDesktopPane getEscritorio() {
+        return escritorio;
+    }
+
+    public void setEscritorio(JDesktopPane escritorio) {
+        this.escritorio = escritorio;
+    }
+
+    public JMenuBar getBarraMenu() {
+        return barraMenu;
+    }
+
+    public void setBarraMenu(JMenuBar barraMenu) {
+        this.barraMenu = barraMenu;
+    }
+
+    public List<JMenu> getMenuList() {
+        return menuList;
+    }
+
+    public void setMenuList(List<JMenu> menuList) {
+        this.menuList = menuList;
+    }
+
+    public ArrayList<JMenuItem> getMenuItemList() {
+        return menuItemList;
+    }
+
+    public void setMenuItemList(ArrayList<JMenuItem> menuItemList) {
+        this.menuItemList = menuItemList;
+    }
+
+    public GestionDato getGd() {
+        return Gd;
+    }
+
+    public void setGd(GestionDato Gd) {
+        this.Gd = Gd;
+    }
+    
 }
